9941bb1fde53c7de9563c111f97e43167c9862ec
slvteblvvk <43097045+slvteblvvk@users.noreply.github.com>
2019-12-04 03:41:08 -0500

    update home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -11,25 +11,26 @@ export default function Home(props) {
     <header>
         <nav>
         <img src={logo} alt='lynk-me logo' width='13%' height='auto' id='img-logo'/>
-          <a href='#'>Home</a>
-          <a href='#'>Art</a>
-          <a href='#'>Connections</a>
-          <a href='#'>Gigs/Events</a>
-          <a href='#'>About</a>
-          <a href='#'>Profile</a>
+          <a href='/home'>Home</a>
+          <a href='/art'>Art</a>
+          <a href='/connections'>Connections</a>
+          <a href='/gigs'>Gigs/Events</a>
+          <a href='/about'>About</a>
+          <a href='/profile'>Profile</a>
       </nav>
     </header>
       <div className ='home'>
       {props.currentUser
           ?
           <>
-            <p>{props.currentUser.username}</p>
-            <button onClick={props.handleLogout}>logout</button>
+            <p id='wel-user'>Welcome {props.currentUser.username}!</p>
+            <button id='logout-btn'onClick={props.handleLogout}>logout</button>
           </>
           :
           <div><h1>You need to log in to do that</h1></div>
         } 
 
+        <h3 id='late-post'>Latest Post...</h3>
       </div>
       </>
 
