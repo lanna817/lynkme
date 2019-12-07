843d314e38bd223e042039111c9084b1c7515d7f
slvteblvvk <43097045+slvteblvvk@users.noreply.github.com>
2019-12-07 01:23:27 -0500

    home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import logo from '../images/lynk_logo.png';
 import PostList from './PostList';
 import CreatePosts from './CreatePosts';
-import Header from './Header';
+import dustin from '../images/dustin_art.jpg';
+import micka from '../images/micka_art.jpg';
+import eli from '../images/eli_art.jpg';
+import xaviera from '../images/xaviera_art.jpg';
+import nrf from '../images/nrf.png';
+import expresso from '../images/expresso.jpg';
 
 
 
@@ -18,9 +23,9 @@ export default function Home(props) {
           :
           <div><h1>You need to log in to do that</h1></div>
         }
-
-        <h3 id='late-post'>Latest Post...</h3>
+      <h3 id='late-post'>Latest Post...</h3>
       <div className='content-home'>
+
       {/* <Header /> */}
       <CreatePosts
         postForm={props.postForm}
@@ -28,7 +33,25 @@ export default function Home(props) {
         createSubmit={props.createSubmit} />
 
       <PostList
-        posts={props.posts} />
+          posts={props.posts} />
+        
+        <div className='local-update'>
+        <h1 id='art-headline'>Artist Spotlight</h1>
+      <div className='locals'>
+        <img src={micka}  alt='artist image' width='20%' />
+        <img src={dustin} alt='artist image' width='30%' />
+        <img src={xaviera} alt='artist image' width='25%'/>
+        <img src={eli} alt='artist image' width='30%' />
+      </div>
+        <>
+        <h1 id='gig-headline'>Latest Gigs/Events</h1>
+      <div className='gigs'>
+      <img src={nrf} alt='events-image' width='20%' />
+       <img src={expresso} alt='events-image' width='20%' />
+      </div>
+        </>
+        </div>
+        
       </div>
       </div>
      );
