6eeb797801d976de9de6e981dbea66888e653cff
slvteblvvk <43097045+slvteblvvk@users.noreply.github.com>
2019-12-09 10:03:22 -0500

    update v3

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -81,14 +81,17 @@ export default function Home(props) {
         </div>
 
       </div>
-        <h3 id='news-headline'>News</h3>
       <div className='news'>
+        <h3 id='news-headline'>News</h3>
         <h3 id='news-title' >ZONA MACO REVEALS GALLERY LIST FOR 2020 EDITION, UNTITLED, ART MIAMI BEACH AWARDS OTAZU ART PRIZE, AND MORE</h3>
-        <p className='news-text'>Zona Maco has announced that more than one-hundred galleries hailing from twenty-six countries in the Americas, Europe, and Asia will participate in its next edition, which will take place in Centro Citibanamex in Mexico City from February 5 to February 9, 2020. For the first time, Zona Maco will present its modern and contemporary art, design, photography, and antiques fairs simultaneously in the same location. </p>
+        <p className='news-text'>Zona Maco has announced that more than one-hundred galleries hailing from twenty-six countries in the Americas, Europe, and Asia will participate in its next edition, which will take place in Centro Citibanamex in Mexico City from February 5 to February 9, 2020. For the first time, Zona Maco will present its modern and contemporary art, design, photography, and antiques fairs simultaneously in the same location...<a href='https://www.artforum.com/news/zona-maco-reveals-gallery-list-for-2020-edition-untitled-art-miami-beach-awards-otazu-art-prize-and-more-81539'>Read More</a></p>
         
         <h3>PÉREZ ART MUSEUM MIAMI ANNOUNCES NADA ACQUISITION GIFT</h3>
         <p className='news-text'>
-        The New Art Dealers Alliance (NADA) and Pérez Art Museum Miami (PAMM) have announced the selection of the third annual NADA acquisition gift for the museum. PAMM associate curator María Elena Ortiz and assistant curator Jennifer Inacio have selected New Hat, 2019, by Dominican-American artist Kenny Rivero from Charles Moffett’s booth for the institution.
+        The New Art Dealers Alliance (NADA) and Pérez Art Museum Miami (PAMM) have announced the selection of the third annual NADA acquisition gift for the museum. PAMM associate curator María Elena Ortiz and assistant curator Jennifer Inacio have selected New Hat, 2019, by Dominican-American artist Kenny Rivero from Charles Moffett’s booth for the institution...<a href='https://www.artforum.com/news/P%C3%A9rez%20Art%20Museum%20Miami%20Announces%20NADA%20Acquisition%20Gift-81550'>Read More</a>
+        </p>
+        <h3>UFTS UNIVERSITY RIDS SACKLER NAME FROM ITS BUILDINGS</h3>
+        <p className='news-text'>Tufts University in Massachusetts will remove the Sackler name from several buildings on its campus, as well as from its programs, over the family’s role in the United States opioid crisis. The school also announced that it will establish a $3 million endowment to support education, research, and civic engagement programs aimed at the prevention and treatment of addiction and substance abuse. Tufts is believed to be the first university to take down the Sacklers’ name...<a href='https://www.artforum.com/news/tufts-university-removes-sackler-name-from-its-buildings-81540'>Read More</a>
         </p>
       </div>
     </div>
