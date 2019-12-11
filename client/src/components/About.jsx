e28338a5f387efb7f75a82c95dda746682be9b53
slvteblvvk <43097045+slvteblvvk@users.noreply.github.com>
2019-12-11 06:52:48 -0500

    new site

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import {profile} from '../images/profile.jpg'
+import profile from '../images/profile.jpg'
 
 
 export default function About() {
   
   return (
     <div className='about-pg'>
+      <h1 id='yoh'>Yohlanna Cort</h1>
+      <img id='me'src={profile} width='50%' alt='image of yohlanna' />
     <h1 id='about-txt'>I created this site to be an improved craiglist geared towards Art. I couldn't do this without
         all the people who were there supporting me and giving me ideas. I would like to thank them for all their help.
         I truly appreciate it!
-      <img src={profile} width='30%'/>
     </h1>
     </div>
   )
